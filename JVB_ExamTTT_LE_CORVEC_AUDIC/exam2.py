8e759b196b6a76e9d4a0043e6f243ccb17df75b1
lcacelia <148343123+lcacelia@users.noreply.github.com>
2023-10-25 15:30:04 +0200

    Add files via upload

---
+++ JVB_ExamTTT_LE_CORVEC_AUDIC/exam2.py
@@ -0,0 +1,12 @@
+#exercice 1
+def grille(tab):
+    tab1 = [0]*3
+    tab2 = [0]*3
+    tab3 = [""]*3
+    tab = [tab1,tab2,tab3]
+    return(tab)
+#ou
+#le code si dessous permet d avoir une vrai grille mais je ne sais comme y acceder
+tab = ["|"]*3
+for i in range (3):
+    print(tab)
